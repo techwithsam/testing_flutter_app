7c7fb9e91cfa1189f6ac69794fe554716abd5afc
Samuel Adekunle <samuelbeebest@gmail.com>
2021-06-19 23:41:17 +0100

    widget test update

---
+++ test/home_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:testing_flutter_app/models/favorites_model.dart';
+import 'package:testing_flutter_app/screens/home.dart';
+
+Widget createHomeScreen() => ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp(home: HomePage()),
+    );
+
+void main() {
+  group('Home Page Widget Tests', () {
+    testWidgets('Testing if ListView shows up', (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.byType(ListView), findsOneWidget);
+    });
+
+    testWidgets('Testing Scrolling', (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.text('Item 0'), findsOneWidget);
+      await tester.fling(find.byType(ListView), Offset(0, -200), 3000);
+      await tester.pumpAndSettle();
+      expect(find.text('Item 0'), findsNothing);
+    });
+  });
+}
